7c0d5ccd23956b49826ec4a526b9d6758021c8d0
0xceejay <0xceejay@gmail.com>
2023-09-17 21:26:10 +0100

    Updated shell file

--- kind.sh
+++ kind.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Install KinD
-go install sigs.k8s.io/kind@v0.20.0
-
 # Create a cluster
 kind create cluster --name ignite
 
 # Set the kubectl context to the new cluster
 kubectl config use-context kind-ignite
 
+# Download the kubeconfig for the cluster and store in kube.yaml file
 kind get kubeconfig --name=ignite > kube.yaml
